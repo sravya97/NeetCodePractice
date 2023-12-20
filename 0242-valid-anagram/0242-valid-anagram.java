893045f6531cafc6a75b987225184f285b0bb097
sravya97 <sravya.gopireddy09@gmail.com>
2023-12-20 14:50:23 -0800

    Time: 6 ms (35.87%), Space: 42.9 MB (70.32%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+        if(s.length() != t.length()){
+            return false;
+        }
+        for(int i=0; i<s.length(); i++){
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            count[sChar - 'a'] ++;
+            count[tChar - 'a'] --;
+        }
+        for(int i=0; i<26; i++){
+            if(count[i] != 0){
+                return false;
+            }
+        }
+        return true;
+        
+        
+    }
+}
